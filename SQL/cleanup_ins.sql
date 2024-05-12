86e82875e2fb5c4f1d1124594fd5a58f11ae146e
pandabadger <33740825+pandabadger@users.noreply.github.com>
2024-05-12 21:25:50 +0100

    update set names

--- SQL/cleanup_ins.sql
+++ SQL/cleanup_ins.sql
@@ -3,7 +3,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 
 INSERT INTO cleanup (clean_id, clean_title, clean_file, clean_time, clean_increment, clean_cron_key, clean_log, clean_desc, clean_on) VALUES
